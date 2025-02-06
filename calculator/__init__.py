234df09ff3bce1feda22cb08d0e18f046cb63785
Bandi Satyanarayana <satyaa6644@gmail.com>
2025-02-06 12:02:07 -0500

    Initial commit

---
+++ calculator/__init__.py
@@ -0,0 +1,15 @@
+"""
+This module provides basic calculator operations such as add, sub, multiplication, division.
+"""
+
+def add(a, b):
+    """
+    Adds two numbers and returns the result.
+    """
+    return a + b
+
+def subtract(a, b):
+    """
+    Subtracts the second number from the first and returns the result.
+    """
+    return a - b
