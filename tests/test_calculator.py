62060056b491bbcb1e3997d27f6204cb145f1643
Satyabandi20 <satyaa6644@gmail.com>
2025-02-17 19:37:36 +0000

    tests written for calculator app

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -1,13 +1,25 @@
-"""
-This module contains unit tests for the calculator module.
-"""
+''' My Calculator Test with Calculator object'''
+import pytest
+from calculator import Calculator
 
-from calculator import add, subtract
+def test_add():
+    '''Tests the addition operation with two decimals passed to the Calculator object'''
+    result = Calculator.add(2,3)
+    assert  result == 5
 
-def test_addition():
-    """Test that the addition function works."""
-    assert add(2, 2) == 4
+def test_subtract():
+    '''Tests the subtraction operation with two decimals passed to the Calculator object'''
+    assert Calculator.subtract(5,3) == 2
 
-def test_subtraction():
-    """Test that the subtraction function works."""
-    assert subtract(2, 2) == 0
+def test_multiply():
+    '''Tests the multiplication operation with two decimals passed to the Calculator object'''
+    assert Calculator.multiply(4,5) == 20
+
+def test_divide():
+    '''Tests the division operation with two decimals passed to the Calculator object'''
+    assert Calculator.divide(10,2) == 5
+
+def test_dividebyzero():
+    '''Tests if the divide function throws an error when divided by zero'''
+    with pytest.raises(ValueError):
+        Calculator.divide(10,0)
